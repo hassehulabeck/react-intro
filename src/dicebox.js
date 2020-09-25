57bc839723630c710aac697a7ee03e0a3f5a1a27
hassehulabeck <hansbudandersson@gmail.com>
2020-09-25 10:35:54 +0200

    temp save

--- src/dicebox.js
+++ src/dicebox.js
@@ -1,11 +1,42 @@
 import React from "react";
+import Dice from "./dice";
 
 export default class Dicebox extends React.Component {
     constructor() {
         super();
-        this.state = {};
+        this.state = {
+            die: [
+                {
+                    value: 1,
+                    locked: false,
+                },
+                {
+                    value: 4,
+                    locked: false,
+                },
+                {
+                    value: null,
+                    locked: false,
+                },
+                {
+                    value: null,
+                    locked: false,
+                },
+                {
+                    value: null,
+                    locked: false,
+                },
+            ],
+        };
     }
     render() {
-        return <div>Dicebox</div>;
+        return (
+            <div>
+                <h1>Dicebox</h1>
+                {this.state.die.map((dice) => {
+                    <Dice value={dice.value} />;
+                })}
+            </div>
+        );
     }
 }
