624f167e4272d49ae78915feb57d3b6348e5c737
hassehulabeck <hansbudandersson@gmail.com>
2020-09-25 09:33:15 +0200

    Added code splitting

---
+++ src/productinfo.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./productinfo.css";
+
+export default function Productinfo(props) {
+    return (
+        <article className="produkt">
+            <h3>{props.product.name}</h3>
+            <p>{props.product.price} inkl moms</p>
+            <button onClick={() => props.klick()}>Ta bort</button>
+        </article>
+    );
+}
