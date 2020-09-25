a47ed9e26b00ad26b9821d22824fdf72f3629071
hassehulabeck <hansbudandersson@gmail.com>
2020-09-25 11:51:46 +0200

    Fixed pair-function and property

--- src/dice.js
+++ src/dice.js
@@ -2,5 +2,17 @@ import React from "react";
 import "./dice.css";
 
 export default function Dice(props) {
-    return <div className="dice">{props.value}</div>;
+    if (props.dice.locked) {
+        return (
+            <div className="dice locked" onClick={() => props.klick()}>
+                {props.dice.value}
+            </div>
+        );
+    } else {
+        return (
+            <div className="dice" onClick={() => props.klick()}>
+                {props.dice.value}
+            </div>
+        );
+    }
 }
--- src/dicebox.js
+++ src/dicebox.js
@@ -8,11 +8,11 @@ export default class Dicebox extends React.Component {
         this.state = {
             die: [
                 {
-                    value: 1,
+                    value: null,
                     locked: false,
                 },
                 {
-                    value: 4,
+                    value: null,
                     locked: false,
                 },
                 {
@@ -28,10 +28,45 @@ export default class Dicebox extends React.Component {
                     locked: false,
                 },
             ],
+            totalSum: 0,
+            combinations: {
+                pair: false,
+                triss: false,
+            },
         };
     }
 
+    componentDidMount() {
+        this.roll();
+    }
+
+    isPair() {
+        for (let i = 1; i <= 6; i++) {
+            let counter = 0;
+            this.state.die.forEach((dice) => {
+                if (dice.value == i) {
+                    counter++;
+                }
+            });
+            if (counter >= 2) {
+                let combinations = this.state.combinations;
+                combinations.pair = true;
+                this.setState({
+                    combinations: combinations,
+                });
+            }
+        }
+    }
+
     roll() {
+        let combinations = this.state.combinations;
+        combinations.pair = false;
+        this.setState({
+            combinations: combinations,
+        });
+
+        let total = 0;
+
         // Trestegsmetod för att ändra i en array i state.
         // 1. Kopiera. 2.Ändra. 3. Lägg tillbaka.
         let tempDice = this.state.die;
@@ -39,7 +74,19 @@ export default class Dicebox extends React.Component {
             if (!dice.locked) {
                 dice.value = Math.ceil(Math.random() * 6);
             }
+            total += dice.value;
         });
+        this.setState({
+            die: tempDice,
+            totalSum: total,
+        });
+        this.isPair();
+    }
+
+    lock(index) {
+        // Trestegsmetod för att ändra i en array i state.
+        let tempDice = this.state.die;
+        tempDice[index].locked = true;
         this.setState({
             die: tempDice,
         });
@@ -49,9 +96,15 @@ export default class Dicebox extends React.Component {
         return (
             <div>
                 <h1>Dicebox</h1>
+                <p>Totalsumma: {this.state.totalSum}</p>
+                <p>{this.state.combinations.pair ? "Par" : "Ej par"}</p>
                 <section className="dicebox">
-                    {this.state.die.map((dice) => (
-                        <Dice value={dice.value} />
+                    {this.state.die.map((dice, index) => (
+                        <Dice
+                            key={index}
+                            dice={dice}
+                            klick={() => this.lock(index)}
+                        />
                     ))}
                 </section>
                 <button onClick={() => this.roll()}>Rulla</button>
